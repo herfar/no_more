91873942795400e81bb4c2730cc4e0d527955e97
herfar <94548763+herfar@users.noreply.github.com>
2022-11-22 19:13:39 +0300

    Add files via upload

---
+++ done/src/test/java/com/bost/done/DoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bost.done;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
